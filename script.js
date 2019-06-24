ec4a14a3d1bf12760dc57116f319631bcf528574
$ git config --global user.name "CRYSTAL_CHEN <42983266+CRYSTALMINYICHEN@users.noreply.github.com>
2019-06-24 16:45:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("div").hide();
+  });
+  $("#show").click(function(){
+    $("div").show();
+      $('#black_overlay').show();
+  });
+});
+
